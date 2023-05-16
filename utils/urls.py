04d9fd7f7273841ea3e8613afb5ea384ef3938db
DefaultUser_2.0 <lawrencemp@yandex.ru>
2023-05-16 15:26:57 +0300

    urls change

--- utils/urls.py
+++ utils/urls.py
@@ -1,4 +1,4 @@
 TENZOR_URL = "https://tensor.ru/"
-SEARCH_URL = "https://ya.ru/"
-SEARCH_RESULTS_URL = "https://yandex.ru/search/"
-IMAGES_URL = "https://yandex.ru/images/"
+YANDEX_SEARCH_URL = "https://ya.ru/"
+YANDEX_SEARCH_RESULTS_URL = "https://yandex.ru/search/"
+YANDEX_IMAGES_URL = "https://yandex.ru/images/"
